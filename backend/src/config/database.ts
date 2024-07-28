5ff031319d8f4543e28bb1a77d15556fc18ccbb7
lcaraballo <lcaraballo@loyal-solutions.com>
2024-07-28 13:56:42 -0300

    FEAT: added .env.template

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -2,9 +2,9 @@ import mysql, { Pool } from 'mysql2'
 import 'dotenv/config'
 
 export const pool: Pool = mysql.createPool({
-    host: process.env.DB_MYSQL_HOST ?? 'localhost',
-    port: Number(process.env.DB_MYSQL_PORT) ?? 3307,
-    user: process.env.DB_MYSQL_USER ?? 'root',
-    password: process.env.DB_MYSQL_PASSWORD ?? 'password',
-    database: process.env.DB_MYSQL_DATABASE ?? 'colegiotp'
+    host: process.env.DB_MYSQL_HOST ?? '',
+    port: Number(process.env.DB_MYSQL_PORT) ?? 1234,
+    user: process.env.DB_MYSQL_USER ?? '',
+    password: process.env.DB_MYSQL_PASSWORD ?? '',
+    database: process.env.DB_MYSQL_DATABASE ?? ''
 })
